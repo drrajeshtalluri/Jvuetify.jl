e0db437af79f5f54c0a94a4259ac266668631bf3
drrajeshtalluri <drrajeshtalluri@gmail.com>
2020-03-23 11:15:34 -0500

    Update prism highlight

--- src/utilfuns.jl
+++ src/utilfuns.jl
@@ -5,14 +5,15 @@ using RCall
 import PlotlyJS
 
 function jhtml(source::String)
-    Weave.weave(source,"pandoc")
+    Weave.weave(source,doctype="pandoc",cache=:user)
     ofile = split(basename(source),".")[1] * ".md"
     htmlfile = split(basename(source),".")[1] * ".html"
     pdir = dirname(@__FILE__)
     vtemplate = joinpath(pdir, "../templates/vuetify.html")
     vcss = read(joinpath(pdir, "../templates/vuetify.css"),String)
     prismjs = read(joinpath(pdir, "../templates/prism.js"),String)
-    run(`pandoc -f markdown -t html $ofile -o $htmlfile --katex --section-divs --template $vtemplate --toc --css $vcss --no-highlight -V prismjs=$prismjs` )
+    pandoc_result = run(`pandoc -f markdown -t html $ofile -o $htmlfile --katex --section-divs --template $vtemplate --toc --css $vcss --no-highlight -V prismjs=$prismjs` );
+    return nothing;
 end
 
 
@@ -22,19 +23,25 @@ function htmltable(df;title="",cnames = names(df))
   end
   trows = df2json(df)
   thead = JSON.json([Dict("value"=>names(df)[i],"text"=>cnames[i]) for i in 1:length(names(df))] )
+  temp_string = ""
+  for i in names(df)
+    temp_string *= """<template v-slot:item.$i="{ item }"><span v-html="item.$i"></span></template>
+    """
+  end
   return """
   ```{=html}
   <vue-data-table>
   <template slot="title"><span style="color:#4e6b95; font-weight:normal">$title</span></template>
   <template slot-scope="{search, cfilter}">
   <v-data-table dense
-      :headers=$thead
-      :items=$trows
+      :headers=\'$thead\'
+      :items=\'$trows\'
       :search = "search"
       :custom-filter = "cfilter"
       multi-sort
       class="elevation-1"
     >
+    $temp_string
     </v-data-table>
   </template>
   </vue-data-table>
